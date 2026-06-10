6ccba4298c1604715130fd5c7d5b2cf0310f7291
mohamedwardi068 <mohamedwardi068@gmail.com>
2026-06-10 19:31:55 +0100

    VF

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import * as SecureStore from 'expo-secure-store';
 // Set this to your computer's IP if testing on a physical device locally (e.g. http://192.168.x.x:5000/api)
 // Set this to your production Render URL for deployment
 const MOBILE_DEV_IP = '192.168.1.17'; // Your local IP found via ipconfig
-const API_URL = `https://busmangerback-2.onrender.com`;
+const API_URL = `https://busmangerback-2.onrender.com/api`;
 //const API_URL = `http://${MOBILE_DEV_IP}:5000/api`;
 
 const api = axios.create({
